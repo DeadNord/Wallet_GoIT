850b3c1a86e94da0307b885ab19a6af15e101238
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2023-08-25 17:04:45 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,7 @@ import authReducer from '../store/auth/auth-slice'
 import storage from 'redux-persist/lib/storage'
 import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, REGISTER, PURGE, PERSIST } from 'redux-persist'
 
-axios.defaults.baseURL = REACT_APP_API_URL
-// axios.defaults.baseURL = 'https://wallet-api-goit.herokuapp.com/api/'
-// axios.defaults.baseURL = 'http://localhost:3005/api/'
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.defaults.withCredentials = true
 
 export const token = {
